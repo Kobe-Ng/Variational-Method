f3d9e786751e34d32481738be9e4cc30faa94f7f
Kobe-Ng <kobeng@live.ca>
2019-03-22 18:06:15 -0700

    switched to numpy for all integration, added new test function

--- Variational Method.py
+++ Variational Method.py
@@ -1,42 +1,47 @@
 import numpy as np
 from sympy import *
+import matplotlib.pyplot as plt
 
 # The test function will have E=C_0*h*w. We only calculate C_0 and multiple hw after. 
-x = Symbol('x')
 h_bar = 1.0
 m = 1.0
 w = 1.0
 
 # test function. Expected expectation value is -0.244845268923
+xfine = np.linspace(-10, 10, 2000)
 a = 0.336796
-A = (a/np.pi)**(0.25)
-psi_test = A*exp(-a*x**2.0/2.0)
-# The values we use to approximate the integral
-xfine = np.linspace(-30, 30, 20000)
 
 
-# TODO: Check if conjugation and differentiation via numpy
-# instead of numpy confers a notable speed up.
-def energy_expectation_value(x, psi, V, xfine):
-    # Set up numpy arrays.
-    psi_conjugate = conjugate(psi)
-    psi_conjugate = lambdify(x, psi_conjugate, 'numpy')
-    psi_conjugate_array = psi_conjugate(xfine)
+def psi_test(x):
+    return exp(-a*x**2.0/2.0)
 
-    psi_second_prime = psi.diff(x, 2)
-    psi_second_prime = lambdify(x, psi_second_prime, 'numpy')
-    psi_second_prime_array = psi_second_prime(xfine)
 
-    psi = lambdify(x, psi, 'numpy')
+def psi_test2(x):
+    return 0.5+0.5*np.tanh(1000*(x+(20/3)**.5)) - (0.5+0.5*np.tanh(1000*(x-(20/3)**.5)))
+
+
+# normalizes a square integrable function.
+def normalize(psi):
+    inner_product = np.trapz(psi * np.conj(psi), xfine)
+    return 1 / (inner_product)**(0.5) * psi
+
+
+def energy_expectation_value(psi, V, xfine):
+    V_array = V(xfine)
     psi_array = psi(xfine)
 
-    V = lambdify(x, V, 'numpy')
-    V_array = V(xfine)
+    psi_array = normalize(psi_array)
+
+    # Set up numpy arrays.
+    psi_conjugate = np.conj(psi_array)
+
+    psi_prime = np.gradient(psi_array, xfine)
+    psi_second_prime = np.gradient(psi_prime, xfine)
 
     # ke_expression is the expression to be integrated related to the p^2/2m term
-    ke_expression = psi_conjugate_array * -h_bar**2.0 / (2.0*m) * psi_second_prime_array 
+    ke_expression = psi_conjugate * -h_bar**2.0 / (2.0*m) * psi_second_prime
     # pe_expression is the expression to be integrated related to the V(x) term
-    pe_expression = psi_conjugate_array * V_array * psi_array
+    pe_expression = psi_conjugate * V_array * psi_array
     kinetic_energy = np.trapz(ke_expression, xfine)
     potential_energy = np.trapz(pe_expression, xfine)
 
@@ -46,5 +51,7 @@ def energy_expectation_value(x, psi, V, xfine):
 def potential(x):
     return -((x**2.0) / 2.0) + ((x**4.0) / 16.0)
 
-V = potential(x)
-print(energy_expectation_value(x, psi_test, V, xfine))
+
+V = potential
+psi = psi_test2
+print(energy_expectation_value(psi, V, xfine))
