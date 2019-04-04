fixed but in regards to calculation of derivative

Expectation Value.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-# The test function will have E=C_0*h*w. We only calculate C_0 and multiple hw after.
+# The test function will have E=C_0*h*w. We only calculate C_0 and multiple hw after. 
 h_bar = 1.0
 m = 1.0
 w = 1.0
@@ -8,10 +8,6 @@ w = 1.0
 # test function. Expected expectation value is -0.244845268923
 xfine = np.linspace(-10, 10, 2000)
 a = 0.336796
-
-
-def potential(x):
-    return -((x**2.0) / 2.0) + ((x**4.0) / 16.0)
 
 
 def psi_test(x):
@@ -22,7 +18,7 @@ def psi_test2(x):
     return 0.5+0.5*np.tanh(1000*(x+(20/3)**.5)) - (0.5+0.5*np.tanh(1000*(x-(20/3)**.5)))
 
 
-# Normalizes a square integrable function.
+# normalizes a square integrable function.
 def normalize(psi):
     inner_product = np.trapz(psi * np.conj(psi), xfine)
     return 1 / (inner_product)**(0.5) * psi
@@ -37,8 +33,9 @@ def energy_expectation_value(psi, V, xfine):
     # Set up numpy arrays.
     psi_conjugate = np.conj(psi_array)
 
-    psi_prime = np.gradient(psi_array, xfine)
-    psi_second_prime = np.gradient(psi_prime, xfine)
+    # xfine[1]-xfine[0] gives the spacing between two elements.
+    psi_prime = np.gradient(psi_array, (xfine[1]-xfine[0]))
+    psi_second_prime = np.gradient(psi_prime, (xfine[1]-xfine[0]))
 
     # ke_expression is the expression to be integrated related to the p^2/2m term
     ke_expression = psi_conjugate * -h_bar**2.0 / (2.0*m) * psi_second_prime
@@ -50,7 +47,10 @@ def energy_expectation_value(psi, V, xfine):
     return kinetic_energy + potential_energy
 
 
-if __name__ == "__main__":
-    V = potential
-    psi = psi_test2
-    print(energy_expectation_value(psi, V, xfine))
+def potential(x):
+    return -((x**2.0) / 2.0) + ((x**4.0) / 16.0)
+
+
+V = potential
+psi = psi_test
+print(energy_expectation_value(psi, V, xfine))